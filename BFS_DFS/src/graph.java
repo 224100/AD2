c33fc953e25d5b94c8ccff01f2fd7c33c0f19da9
2241001030 <160648712+2241001030@users.noreply.github.com>
2024-03-06 00:16:10 +0530

    Update graph.java

--- BFS_DFS/src/graph.java
+++ BFS_DFS/src/graph.java
@@ -1,33 +1,33 @@
 import java.util.*;
 public class graph
 {
-    private LinkedList <Integer> adjacencyList[];
+    private LinkedList <Integer> adj[];
     public graph(int v)
     {
-        adjacencyList=new LinkedList[v];
+        adj=new LinkedList[v];
         for(int i=0;i<v;i++)
         {
-            adjacencyList[i]=new LinkedList<Integer>();
+            adj[i]=new LinkedList<Integer>();
         }
     }
     public void insert_edge(int x,int y)
     {
-        adjacencyList[x].add(y);
-        adjacencyList[y].add(x);
+        adj[x].add(y);
+        adj[y].add(x);
     }
     void dfs(int s)
     {
-        boolean flag[]=new boolean[adjacencyList.length];
+        boolean flag[]=new boolean[adj.length];
         Stack<Integer> stack=new Stack<Integer>();
         stack.add(s);
         flag[s]=true;
         while(!stack.empty())
         {
-            int adj=stack.pop();
-            System.out.print(adj+"  ");
-            for(int i:adjacencyList[adj])
+            int a=stack.pop();
+            System.out.print(a+"  ");
+            for(int i:adj[a])
             {
-                if(flag[i]==false)
+                if(!flag[i])
                 {
                     flag[i]=true;
                     stack.add(i);
@@ -38,17 +38,17 @@ public class graph
     }
     void bfs(int s)
     {
-        boolean flag[]=new boolean[adjacencyList.length];
+        boolean flag[]=new boolean[adj.length];
         Queue<Integer> q=new LinkedList<Integer>();
         q.add(s);
         flag[s]=true;
         while(!q.isEmpty())
         {
-            int adj=q.remove();
-            System.out.print(adj+"  ");
-            for(int i:adjacencyList[adj])
+            int a=q.remove();
+            System.out.print(a+"  ");
+            for(int i:adj[a])
             {
-                if(flag[i]==false)
+                if(!flag[i])
                 {
                     flag[i]=true;
                     q.add(i);
