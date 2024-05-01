1575e5ccf96431a67206a4b496ea5e4b67fe1090
224100 <gentlecode45@gmail.com>
2024-05-01 17:32:22 +0530

    1-5

---
+++ divrule/src/counting_inversion.java
@@ -0,0 +1,54 @@
+public class counting_inversion {
+    public static void main(String[] args) {
+        int[] a = {1, 20, 6, 4, 5};
+        int n = a.length;
+        System.out.println("Number of inversions are " + mergeSort(a, 0, n - 1));
+    }
+    static int mergeSort(int[] a, int l, int r) {
+        int count = 0;
+        if (l < r) {
+            int m = (l + r) / 2;
+            count += mergeSort(a, l, m);
+            count += mergeSort(a, m + 1, r);
+            count += merge(a, l, m, r);
+        }
+        return count;
+    }
+    static int merge(int[] a, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+        for (int i = 0; i < n1; i++) {
+            L[i] = a[l + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            R[j] = a[m + 1 + j];
+        }
+        int i = 0, j = 0;
+        int k = l;
+        int count = 0;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                a[k] = L[i];
+                i++;
+            } else {
+                a[k] = R[j];
+                j++;
+                count += n1 - i;
+            }
+            k++;
+        }
+        while (i < n1) {
+            a[k] = L[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            a[k] = R[j];
+            j++;
+            k++;
+        }
+        return count;
+    }
+}
---
+++ divrule/src/ms.java
@@ -0,0 +1,55 @@
+public class ms {
+
+    public static void main(String[] args) {
+        int[] a = {12, 11, 13, 5, 6, 7};
+        int n = a.length;
+        mergeSort(a, 0, n - 1);
+        System.out.println("Sorted array is:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+    static void mergeSort(int[] a, int l, int r) {
+        if (l < r) {
+            int m = (l + r) / 2;
+            mergeSort(a, l, m);
+            mergeSort(a, m + 1, r);
+            merge(a, l, m, r);
+        }
+    }
+    static void merge(int[] a, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+        for (int i = 0; i < n1; i++) {
+            L[i] = a[l + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            R[j] = a[m + 1 + j];
+        }
+        int i = 0, j = 0;
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                a[k] = L[i];
+                i++;
+            } else {
+                a[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1) {
+            a[k] = L[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            a[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+}
