1cb2030e80fb54b3dc9717d1d5ddea9508bb649a
224100 <160648712+224100@users.noreply.github.com>
2024-05-08 17:21:20 +0530

    8-5

--- divrule/src/QS.java
+++ divrule/src/QS.java
@@ -1,29 +1,50 @@
+import java.util.Scanner;
+
 public class QS {
     public static void main(String[] args) {
-        int[] a = {9,5,8,14,3,6,7,14,41,16,56,26,1};
+
+        Scanner sc =new Scanner(System.in);
+
+        System.out.println("Enter the size : ");
+        int[] a = new int[sc.nextInt()];
+
+        System.out.println("Enter the elements : ");
+        for(int i =0;i<a.length;i++)
+            a[i]=sc.nextInt();
+
         quicksort(a,0,a.length-1);
-        for(int i : a){
+
+        for(int i : a)
             System.out.print(i+" , ");
-        }
+
     }
     static int partition(int[]a , int p,int r){
+
         int pivot = a[r];
         int i = p-1;
-        for(int j = p ;j<=r-1;j++ ){
+
+        for(int j = p ;j<=r-1;j++ )
             if(a[j]<=pivot){
                 i++;
                 swap(a,i,j);
             }
-        }
+
+
         swap(a,i+1,r);
         return(i+1);
+
     }
+
     static void swap(int[]a , int i,int j){
+
         int temp = a[i];
         a[i]=a[j];
         a[j]=temp;
+
     }
+
     static void quicksort(int[]a,int p, int r){
+
         if(p<r){
             int m =partition(a,p,r);
 
